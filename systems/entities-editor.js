9dc4ab5c70332b7780bdc961275cd0bea67a2fad
Angel <aj.abregu@gmail.com>
2023-05-07 03:12:36 -0300

    Fix modo inicial del editor

--- systems/entities-editor.js
+++ systems/entities-editor.js
@@ -1,4 +1,4 @@
-import { Toolbar, ACTION_EDIT_PROPERTY, ACTION_ADD_ENTITY, ACTION_REMOVE_ENTITY } from './ui/toolbar.js';
+import { Toolbar, ACTION_EDIT_PROPERTY, ACTION_ADD_ENTITY, ACTION_REMOVE_ENTITY, NO_ACTION } from './ui/toolbar.js';
 
 AFRAME.registerSystem('entities-editor', {
     init: function () {
@@ -16,34 +16,33 @@ AFRAME.registerSystem('entities-editor', {
             enabledComponent: '',
             currentMode: null
         }
+        let _initialMode = this.ui.toolbar.getSelectedAction();
+        if (_initialMode !== NO_ACTION) {
+            this.setMode(_initialMode);
+        }
     },
   
-    changeMode: function(newMode) {
+    setMode: function(newMode) {
+        // Quita componente con accion actual
         this.supportingElement.removeAttribute(this.state.enabledComponent);
 
+        // Agrega el componente correspondiente a la acción elegida
         switch (newMode) {
             case ACTION_EDIT_PROPERTY:
-                this.state = {
-                    enabledComponent: 'edit-properties',
-                    currentMode: ACTION_EDIT_PROPERTY
-                };
+                this.state.enabledComponent = 'edit-properties';
                 break;
             case ACTION_ADD_ENTITY:
-                this.state = {
-                    enabledComponent: 'add-entities',
-                    currentMode: ACTION_ADD_ENTITY
-                };
+                this.state.enabledComponent = 'add-entities';
                 this.supportingElement.setAttribute('add-entities', 'primitive', this.ui.toolbar.getSelectedPrimitive());
                 break;
             case ACTION_REMOVE_ENTITY:
-                this.state = {
-                    enabledComponent: 'remove-entities',
-                    currentMode: ACTION_REMOVE_ENTITY
-                };
+                this.state.enabledComponent = 'remove-entities';
                 break;
             default:
                 console.warn('Unknown action');
         }
+        // Establece el modo actual
+        this.state.currentMode = newMode;
     },
 
     changePrimitive: function(newPrimitive) {
--- systems/ui/toolbar.js
+++ systems/ui/toolbar.js
@@ -1,24 +1,27 @@
-export const ACTION_EDIT_PROPERTY = 0;
-export const ACTION_ADD_ENTITY = 1;
-export const ACTION_REMOVE_ENTITY = 2;
+export const NO_ACTION = 0;
+export const ACTION_EDIT_PROPERTY = 1;
+export const ACTION_ADD_ENTITY = 2;
+export const ACTION_REMOVE_ENTITY = 3;
 
 export function Toolbar(system) {
-    this.mode = ACTION_ADD_ENTITY;
+    this.actions = {
+        'action-edit-properties': ACTION_EDIT_PROPERTY,
+        'action-add-entity': ACTION_ADD_ENTITY,
+        'action-remove-entity': ACTION_REMOVE_ENTITY
+    };
+
     this.selectedPrimitive = document.querySelector('#add-entity-list option:checked').value;
 
     document.querySelector('#action-edit-properties').addEventListener('click', (event) => {
-        this.mode = ACTION_EDIT_PROPERTY;
-        system.changeMode(ACTION_EDIT_PROPERTY);
+        system.setMode(ACTION_EDIT_PROPERTY);
     });
    
     document.querySelector('#action-add-entity').addEventListener('click', (event) => {
-        this.mode = ACTION_ADD_ENTITY;
-        system.changeMode(ACTION_ADD_ENTITY);
+        system.setMode(ACTION_ADD_ENTITY);
     });
     
     document.querySelector('#action-remove-entity').addEventListener('click', (event) => {
-        this.mode = ACTION_REMOVE_ENTITY;
-        system.changeMode(ACTION_REMOVE_ENTITY);
+        system.setMode(ACTION_REMOVE_ENTITY);
     });
 
     document.querySelector('#add-entity-list').addEventListener('change', (event) => {
@@ -27,5 +30,11 @@ export function Toolbar(system) {
     });
 
     this.getSelectedPrimitive = () => this.selectedPrimitive;
-    this.getMode = () => this.mode;
+    this.getSelectedAction = () => {
+        let selectedOption = document.querySelector('#app-toolbar [name="actions"]:checked');
+        if (selectedOption !== null) {
+            return this.actions[selectedOption.value];
+        }
+        return NO_ACTION
+    };
 }
